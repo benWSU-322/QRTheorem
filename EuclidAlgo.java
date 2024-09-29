555fd8c57e12f6bada43c6e2b89965d3e2e4adae
benWSU-322 <benjamin.m.burns@wsu.edu>
2024-09-29 14:15:00 -0700

    Working (needs 1 more case)
    Works assuming the two numbers have a GCD, needs to be able to handle when there is no GCD (cant depend on user to use program correctly lol)

--- EuclidAlgo.java
+++ EuclidAlgo.java
@@ -8,35 +8,43 @@ public class EuclidAlgo{
         //find GCD of A and B
         //Currently Assumes A > B and that 
         int  A = 0, B = 0;
+        System.out.println("Enter two numbers to find GCD of the two");
         Scanner read = new Scanner(System.in);
         do{
-            System.out.print("Please enter a Dividend:");
+            System.out.print("Please enter an integer: ");
             String AUnchecked = read.next();
             if(inputValidation.isNumberic(AUnchecked)){
                 A = Integer.parseInt(AUnchecked);
             }else{
-                System.out.print("confirm your dividen is a integer");
+                System.out.println("Confirm your input is a positive integer");
             }
         }while( A == 0);
         do { 
-            System.out.print("Please select a Divisor:");
+            System.out.print("Please enter an integer: ");
             String BUnchecked = read.next();
             if(inputValidation.isNumberic(BUnchecked)){
                 B = Integer.parseInt(BUnchecked);
             }else{
-                System.out.print("confirm your divisor is non-zero");
+                System.out.println("Confirm your input is a posiivitve integer");
             }  
         } while (B == 0);
+
+        int result = AlgoStart(A,B,A%B);
+        System.out.print(result);
+        
     }
 
 
 
-    public static int AlogStart(int A, int B, int R){
+    public static int AlgoStart(int A, int B, int R){
         int[] result = Algo(A,B,R);
         return result[0];
     }
     public static int[] Algo(int A, int B, int R){
-        if(R==0) 
+        // if (R == 0 && B == 0) {
+        //     return new int[] {-1,-1,-1};
+        // }
+        if(B == 0) 
             return new int[] {A,B,R};
         else{
             return Algo(B, R, A%B);
